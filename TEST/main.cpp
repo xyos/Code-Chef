d15eb8dc22ab2bd8e4a613cc7e2d5f1fe45d05c0
xyos <slayerjairo@gmail.com>
2011-10-10 02:44:39 -0500

    holes

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main()
 {
 	int e;
-	int t = 1;
-	vector<int> r;
 	while(cin >> e){
-		if(t && e != 42){
-			r.push_back(e);
+		if(e != 42){
+			cout << e;
 		}else{
-			t = 0;
+			break;
 		}
 	}
-	for(vector<int>::size_type i = 0; i != r.size() - 1; i++) {
-		cout << r[i] << endl;
-	}
-	cout << r[r.size() -1];
 	return 0;
 }
