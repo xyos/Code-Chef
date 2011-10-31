ebe344792661951f4a6d966cb152e7cf5d4c0501
xyos <slayerjairo@gmail.com>
2011-10-30 21:21:56 -0500

    min should be checked while reading the numbers

--- RECIPE/main.cpp
+++ RECIPE/main.cpp
@@ -6,14 +6,7 @@ void divide(int * &v, const int &n, const int &s){
 		v[i] = v[i]/n;
 	}
 }
-void gcd(int* &v, const int &s){
-	int m = v[0];
-	//finding min in array
-	for(int i = 1; i < s;i++){
-		if(v[i] < m){
-			m = v[i];
-		}
-	}
+void gcd(int* &v, const int &s, const int &m){
 	//finding gcd and processing array
 	for(int i = m; i > 2; i--){
 		int count = 0;
@@ -37,15 +30,17 @@ void gcd(int* &v, const int &s){
 int main()
 {
 	int n, i, size;
-    int* v;
+    int* v;//each number [1-1000]
 	cin >> n;
 	while(n--){
         scanf("%d", &size);
         v = new int[size];
+		int min = 1001;
 		for(int i = 0; i < size; i++){
             scanf("%d", &v[i]);
+			if(v[i]<min) min = v[i];
 		}
-        gcd(v,size);
+        gcd(v,size,min);
 	}
 	return 0;
 }
